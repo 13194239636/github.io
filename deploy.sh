ebfb52bfdb6d32489750cacb6c91b9904430cb12
13194239636 <1129391977@qq.com>
2024-07-06 23:25:44 +0800

    测试hello12

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 message=$1
+# 读取当前目录是否有node_modules文件夹,若没有就执行npm install --force命令
+if [ ! -d "./node_modules" ]; then
+  npm install --force
+fi
 hexo g
 rm -rf ./docs
 mv ./public ./docs
